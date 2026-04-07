1f04ca143218cb423a63ae0a99e7eac13ce407f8
baltasarblanco <baltablanco9008@gmail.com>
2026-04-07 01:28:46 -0300

    feat: setup Reservation models, schemas and database migration

--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
-from sqlalchemy import Boolean, Column, Integer, String
-from .database import Base
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
+from datetime import datetime, timezone
+from app.database import Base
 
 class ArmaduraDB(Base):
     # El nombre real que tendrá la tabla en la base de datos
@@ -18,3 +20,18 @@ class UsuarioDB(Base):
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String) # ¡Acá va el hash, no la clave real!
     is_active = Column(Boolean, default=True)
+
+class ReservaDB(Base):
+    __tablename__ = "reservas"
+
+    id = Column(Integer, primary_key=True, index=True)
+    # Claves foráneas que conectan esta isla con las otras dos
+    usuario_id = Column(Integer, ForeignKey("usuarios.id"), nullable=False)
+    # Asumo que la armadura se identifica por un ID o un Modelo (ajustá a String si tu PK de armaduras es el modelo)
+    armadura_modelo = Column(String, ForeignKey("armaduras.modelo"), nullable=False) 
+    
+    # Tiempo Universal (timezone=True garantiza que PostgreSQL lo entienda con zona horaria)
+    fecha_inicio = Column(DateTime(timezone=True), nullable=False)
+    fecha_fin = Column(DateTime(timezone=True), nullable=False)
+    
+    estado = Column(String, default="activa") # Puede ser: activa, finalizada, cancelada
